6f72c28075a7f0248511a5399ad76b0a5b3de100
mutzon <martinutzon@gmail.com>
2024-10-21 11:28:17 +0200

    Styling_19

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { s } from "./App.style";
 export default function App() {
-  return;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View style={s.square}>
+          <Text style={s.innerTxt}>Super</Text>
+        </View>
+        <Text>hello</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
---
+++ App.style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+export const s = StyleSheet.create({
+  title: {
+    color: "red",
+    fontSice: "100",
+    marginTop: "50",
+    marginLeft: "50%",
+  },
+
+  square: {
+    height: 100,
+    backgroundColor: "green",
+    width: "100",
+  },
+  innerTxt: {
+    color: "white",
+    marginTop: 30,
+    marginLeft: 40,
+  },
+});
