7487c1e12ea668e46ae6dfc7a6b6ced0ed9e7530
mutzon <martinutzon@gmail.com>
2024-10-27 12:32:08 +0100

    færdig med states

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { ProfilCard } from "./components/Profilcard/ProfilCard";
+import { AgeCounter } from "./components/AgeCounter/AgeCounter";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, padding: 20 }}>
-        <ProfilCard
+        <AgeCounter></AgeCounter>
+        {/*         <ProfilCard
           firstName={"Martin"}
           lastName={"utzon"}
           age={30}
-        ></ProfilCard>
+        ></ProfilCard> */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ components/AgeCounter/AgeCounter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+export function AgeCounter() {
+  const [smuk, setAge] = useState(30);
+
+  function increaseAge() {
+    setAge(smuk + 1);
+  }
+  console.log("I render" + smuk);
+  return (
+    <>
+      <TouchableOpacity
+        onPress={increaseAge}
+        style={{ backgroundColor: "#00A2ff" }}
+      >
+        <Text style={{ fontSize: 40, color: "white" }}>Increase age</Text>
+        <TouchableOpacity>
+          <Text style={{ fontSize: 40 }}>I am {smuk} years old</Text>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    </>
+  );
+}
